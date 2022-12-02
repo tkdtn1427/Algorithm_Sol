51dd8b6ee33ef4bed582a3f9222aae9ac821a76a
[Sangsoo Lee] <tkdtn1427@gmail.com>
2022-12-02 14:32:24 +0900

    dailyCoding

---
+++ backJoon/가장긴증가하는부분수열.java
@@ -0,0 +1,70 @@
+package backJoon;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+// 실버2
+//
+public class 가장긴증가하는부분수열 {
+    static int[][] count;
+    public static void main(String[] args) throws Exception{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        int repeat = Integer.parseInt(br.readLine());
+//        int result = 0;
+//        String str = br.readLine();
+//        StringTokenizer st = new StringTokenizer(str, " ");
+//        count = new int[repeat][2];
+//
+//        for(int i=0; i<repeat; i++) {
+//            int num = Integer.parseInt(st.nextToken());
+//            count[i][0] = num;
+//            int max = 0;
+//            for(int j=0; j<i; j++){
+//                if(count[j][0] < num && count[j][1] > max){
+//                    max = count[j][1];
+//                }
+//            }
+//            count[i][1] = max+1;
+//            if(result < count[i][1]){
+//                result = count[i][1];
+//            }
+//        }
+//
+//        bw.write(String.valueOf(result));
+//        bw.flush();
+//        bw.close();
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        int N = Integer.parseInt(st.nextToken());
+        st = new StringTokenizer(br.readLine());
+
+        ArrayList<Integer> dp = new ArrayList<>();
+        dp.add(0);
+        for (int i = 0 ; i<N; i++){
+            int num = Integer.parseInt(st.nextToken());
+            if (num>dp.get(dp.size()-1)){
+                dp.add(num);
+            }else{
+                int idx = Collections.binarySearch(dp,num);
+                System.out.println(idx);
+                if (idx<0){
+                    idx = -(idx+1);
+                    System.out.println();
+                    System.out.println(idx);
+                }
+                dp.set(idx,num);
+            }
+            System.out.println(dp);
+        }
+        System.out.println(dp.size()-1);
+    }
+}
